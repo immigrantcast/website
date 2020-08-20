ed88376525e4b5ce89e743c3ab5087fdd0ff899c
Artur Paikin <artur@arturpaikin.com>
2020-08-20 19:55:43 +0100

    add redirects and excerpt

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,15 @@
 const Typograf = require('typograf')
 const tp = new Typograf({locale: ['ru', 'en-US']})
+const excerpt = require('eleventy-plugin-excerpt')
 
 module.exports = function (config) {
   config.addTransform('typograph', function(content, outputPath) {
       return tp.execute(content)
   })
 
+  eleventyConfig.addPlugin(excerpt)
+
+  config.addPassthroughCopy('src/_redirects');
   // config.addPassthroughCopy('src/fonts');
   // config.addPassthroughCopy('src/images');
   // config.addPassthroughCopy('src/styles');

2c2fb88dfd44a7ec78b963dbabb96c6df536f632
Artur Paikin <artur@arturpaikin.com>
2020-08-27 13:34:39 +0100

    support more time formats, switch to #t=
    /#t=1:02:30

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -9,16 +9,37 @@
 function setEpisodeAudioTime (seconds) {
   let audioPlayer = document.querySelector('#episode-audio-player')
   audioPlayer.currentTime = seconds
-
-  // attempt to auto play audio, won’t work in most 
-  setTimeout(() => { 
-    audioPlayer.play()
-  }, 1000)
 }
 
-let urlParams = new URLSearchParams(window.location.search)
-let timeString = urlParams.get('time')
+function getHashTimeVariable () {
+  let regex = /\bt=([\dhHmMsS.:]*)(?:,([\dhHmMsS.:]+))?\b/g
+  let match = regex.exec(location.hash)
+  if (match) {
+    return match[1]
+  }
+  return false
+}
+
+function parseTime (str) {
+  let plain = /^\d+(\.\d+)?$/g
+  let npt = /^(?:npt:)?(?:(?:(\d+):)?(\d\d?):)?(\d\d?)(\.\d+)?$/
+  let quirks = /^(?:(\d\d?)[hH])?(?:(\d\d?)[mM])?(\d\d?)[sS]$/
+
+  if (plain.test(str)) {
+    return parseFloat(str);
+  }
+
+  let match = npt.exec(str) || quirks.exec(str)
+  if (match) {
+    return (3600 * (parseInt(match[1],10) || 0) + 60 * (parseInt(match[2],10) || 0) + parseInt(match[3],10) + (parseFloat(match[4]) || 0))
+  }
+  return 0
+};
+
+let timeString = getHashTimeVariable()
 if (timeString) {
-  setEpisodeAudioTime(timeString)
+  setEpisodeAudioTime(
+    parseTime(timeString)
+  )
 }
 
